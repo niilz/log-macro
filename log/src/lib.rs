7e2670276cb157e59f62ad63985070a64ac101e2
nilSinger <niilz@outlook.de>
2021-10-30 13:59:56 +0200

    Implemented 'print exit of function' before it returns

--- log/src/lib.rs
+++ log/src/lib.rs
@@ -2,19 +2,32 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, ItemFn, ReturnType};
 
 #[proc_macro_attribute]
 pub fn log(_args: TokenStream, input: TokenStream) -> TokenStream {
     let function = parse_macro_input!(input as ItemFn);
     let signature = function.sig;
     let func_name = &signature.ident.to_string();
-    let body = function.block;
+    let mut body = function.block;
+
+    // Figure out if the function is returning a type
+    // If so: store the last statement separately
+    let return_type = match signature.output {
+        ReturnType::Default => quote!(()),
+        ReturnType::Type(_, _) => match body.stmts.pop() {
+            Some(stmt) => quote!(#stmt),
+            None => unreachable!("There must be a statement if there is a return_type"),
+        },
+    };
 
     quote!(
         #signature {
             println!("> {}", #func_name);
             #body
+            println!("< {}", #func_name);
+
+            #return_type
         }
     )
     .into()
